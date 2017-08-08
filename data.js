dfc686e82958e15ff01e2afe77b502f804a0c3bf
Ali Karami <Ali.karami.m@gmail.com>
2017-08-08 22:43:08 +0430

    Initial commit

---
+++ data.js
@@ -0,0 +1,103 @@
+var pgpkey = "hKRib2R5hqhkZXRhY2hlZMOpaGFzaF90eXBlCqNrZXnEIwEgBF9WcmTT6h7TOeWD9H5/r9kP8Yp7VbK8KZ/zhppY9n4Kp3BheWxvYWTFAs57ImJvZHkiOnsiZGV2aWNlIjp7ImlkIjoiZmJiM2I1OWQ5NmNhYTY5NTcwODI0YWNjNGViNThmMTgiLCJraWQiOiIwMTIwMDQ1ZjU2NzI2NGQzZWExZWQzMzllNTgzZjQ3ZTdmYWZkOTBmZjE4YTdiNTViMmJjMjk5ZmYzODY5YTU4ZjY3ZTBhIiwibmFtZSI6IkFsaUJvb2sgUHJvIiwic3RhdHVzIjoxLCJ0eXBlIjoiZGVza3RvcCJ9LCJrZXkiOnsiaG9zdCI6ImtleWJhc2UuaW8iLCJraWQiOiIwMTIwMDQ1ZjU2NzI2NGQzZWExZWQzMzllNTgzZjQ3ZTdmYWZkOTBmZjE4YTdiNTViMmJjMjk5ZmYzODY5YTU4ZjY3ZTBhIiwidWlkIjoiNmZkZmQxYzdiODMyMWZmOTI0NGVjNTA2ZGM1Mjg3MTkiLCJ1c2VybmFtZSI6ImFsaWthcmFtaSJ9LCJ0eXBlIjoiZWxkZXN0IiwidmVyc2lvbiI6MX0sImNsaWVudCI6eyJuYW1lIjoia2V5YmFzZS5pbyBnbyBjbGllbnQiLCJ2ZXJzaW9uIjoiMS4wLjIzIn0sImN0aW1lIjoxNDk3MDQ4MDU0LCJleHBpcmVfaW4iOjUwNDU3NjAwMCwibWVya2xlX3Jvb3QiOnsiY3RpbWUiOjE0OTcwNDc4MDAsImhhc2giOiJiZTcwODU1NThmZmM5MTg4MjYzZDkwOGUzY2ZkOTZhYjhiYTM1OTYxMGMxYjgwZTQ1MjQwODRhZTZiNjlmYmVkOTRmZjNkYzViZTJhMjI4MTk0MDBmZWZlZTE0NjRjNTVkNWU3NjExNDBiYjI0NGY1ZjBhZGI1MzMwMzg5N2YxYiIsInNlcW5vIjoxMTU4OTAwfSwicHJldiI6bnVsbCwic2Vxbm8iOjEsInRhZyI6InNpZ25hdHVyZSJ9o3NpZ8RAjSWrzeqArxI1DlurmXhBYNhrHHwzLRdVB8KDGT8l7RLl6mHZhIig8HUEVZBEakNTsY0sZJXg+hXhaDM1GlBuCahzaWdfdHlwZSCkaGFzaIKkdHlwZQildmFsdWXEIP27TjDE+3Gdel1XnI+7ijqt7CNl7V0WxO3n7uwR4gzVo3RhZ80CAqd2ZXJzaW9uAQ==";
+var resumeUrl = './resume.pdf';
+var greetingMsg = "Welcome to [[g;#ffaa00;]Ali Karami]'s Website.\n\nType [[bu;#;]help] for commands\n";
+var myLocation = 'Tehran, Iran.'
+var mans = {
+	help: 'lists help for all the commands',
+	man: 'describes what each command does',
+	echo: 'monkey command repeats everything',
+	clear: 'clear command history from screen',
+	pgpkey: 'print PGP key',
+	about: 'about me',
+	contact: 'contact information',
+	location: 'current location',
+	socialmedia: 'social media profiles',
+	resume: 'download pdf version of résumé',
+	skills: 'skills obtained',
+	education: 'education history',
+	employment: 'employment history',
+	volunteering: 'volunteering history',
+	languages: 'languages spoken',
+	interests: 'list of interests',
+	github: 'list Github repositories',
+	js: 'javascript interpreter'
+};
+var contacts = {
+	Email: 'ali.karami.m@gmail.com',
+	Mobile: '+98 919 555 0832'
+};
+var social = {
+	Twitter: 'https://twitter.com/AliKarami_',
+	Facebook: 'https://www.facebook.com/ali.karami.m',
+	Reddit: 'https://www.reddit.com/user/AliKarami/',
+	Telegram: 'https://t.me/ali_karami',
+	Instagram: 'https://www.instagram.com/alikarami_/'
+};
+var skills = {
+	'Main Skill': ['NodeJS'],
+	'Skilled': ['C','C++','Java','Javascript','Git & Version Controlling'],
+	'Familiar': ['Python','HTML, CSS','Polymer Project','JSP & Servlets','Software Architecture','SQL & noSQL Databases','RabbitMQ','Websockets','Iranian Core-Banking Systems','Content and Learning Management Systems'],
+	'Knowledge': ['Cryptocurrencies','Unix-based Operating Systems','Machine Learning','Design Basics']
+};
+var education = {
+	Bachelor: {
+		institution: 'University of Tehran',
+		period: '2012-Pres',
+		location: 'Tehran'
+	},
+	Diploma: {
+		institution: 'Mofid 1 Highschool Complex',
+		period: '2007-2012',
+		location: 'Tehran'
+	},
+	School: {
+		institution: 'Rouzbeh Educational Complex',
+		period: '1998-2007',
+		location: 'Tehran'
+	}
+};
+var employment = [{
+	position: 'Backend Developer',
+	site: 'Snapp',
+	location: 'Tehran',
+	period: '2017-Pres'
+},{
+	position: 'Backend Developer',
+	site: 'WING (Word of Innovative Geniuses)',
+	location: 'Tehran',
+	period: '2016-2017'
+},{
+	position: 'Trainee',
+	site: 'MediaTools',
+	location: 'Tehran',
+	period: '2016-2016'
+}];
+var volunteering = [{
+	type: 'Charity',
+	name: 'VARES Charity Jouney Group',
+	position: 'Manager',
+	period: '2014-2016'
+},{
+	type: 'Student Association',
+	name: 'Islamic Association of Electrical and Computer Engineering School of Tehran University',
+	position: 'Manager',
+	period: '2014-2016'
+}];
+var languages = [{
+	lang: 'Persian',
+	level: 'Mother Tongue'
+},{
+	lang: 'English',
+	level: 'Fluent'
+}];
+var interests = ['Bitcoin and other Cryptocurrencies',
+	'Machine Learning',
+	'Neural Networks',
+	'Human-Computer Interaction',
+	'Internet of Things and Smart Home',
+	'Semantic Web',
+	'Politics and Society',
+	'Theater and Cinema',
+	'Football'];
+	var aboutMe = "always sleepy, always hungry";
+var githubUrl = 'https://github.com/AliKarami';
---
+++ main.js
@@ -0,0 +1,135 @@
+jQuery(function($, undefined) {
+    $('body').terminal({
+    	echo: function() {
+    		var words = [];
+    		for (var i=0,len=arguments.length;i<len;i++)
+    			words.push(arguments[i]);
+    		this.echo(words.join(' '));
+    	},
+    	help: function() {
+    		var _this = this;
+    		for (var command in mans)
+    			this.echo('[[ugb;;]' + command + ']' + ' - ' + mans[command]);
+    	},
+    	pgpkey: function() {
+    		this.echo("-----BEGIN PGP PUBLIC KEY BLOCK-----");
+    		this.echo(pgpkey);
+    		this.echo("-----END PGP PUBLIC KEY BLOCK-----");
+    	},
+    	man: function() {
+    		var _this = this;
+    		if (arguments[0] && mans[arguments[0]])
+    			_this.echo('[[ugb;;]' + arguments[0] + ']' + ' - ' + mans[arguments[0]]);
+    		else if (arguments[0]){
+    			_this.error("'" + arguments[0] + "'" + "is not valid command.")
+    		}
+    	},
+    	about: function() {
+    		this.echo(aboutMe);
+    	},
+    	contact: function() {
+    		for (var contact in contacts) {
+    			var key = "[[gb;;]● " + contact + ":\t]";
+    			var value = "[[!u;#00a5ff;]" + contacts[contact] + "]";
+    			this.echo(key + value);
+    		}
+    	},
+    	resume: function() {
+    		this.echo("opening résumé.pdf")
+			setTimeout(function(){
+					window.open(resumeUrl,'_blank');
+				}, 1000);
+    	},
+    	location: function() {
+    		this.echo(myLocation);
+    	},
+    	socialmedia: function() {
+    		for (var media in social) {
+    			var key = "[[gb;;]● " + media + ":]";
+    			var value = "[[!u;#00a5ff;]" + social[media] + "]";
+    			this.echo(key + (media.length<7?'\t\t\t':'\t\t') + value);
+    		};
+    	},
+    	skills: function() {
+    		var _this = this;
+    		for (var category in skills) {
+    			var key = "[[b;;]● " + category + ":]";
+    			var values = [];
+    			skills[category].forEach(function(skill) {
+    				values.push("[[i;;]" + skill + "]");
+    			});
+    			_this.echo(key);
+    			values.forEach(function(skill) {
+    				_this.echo('\t-' + skill);
+    			});
+    		};
+    	},
+    	education: function() {
+    		var _this = this;
+    		for (var edu in education) {
+    			var key = "[[b;;]● " + edu + ":]";
+    			var value = "[[u;;]" + education[edu].institution + "]" + (education[edu].institution.length<25?"\t\t":"\t") +
+    				"[[i;;]" + education[edu].location + "]" + "\t" +
+    				"[[g;;]" + education[edu].period + "]";
+    			_this.echo(key + '\t\t' + value);
+    		}
+    	},
+    	employment: function() {
+    		var _this = this;
+    		employment.forEach(function(emp) {
+    			_this.echo("[[i;;]" + emp.period + "]" + "\t" + "[[b;;]" + emp.position + "]" + (emp.position.length<11?"\t\t\t":"\t") + emp.site + "\t" + emp.location);
+    		});
+    	},
+    	volunteering: function() {
+    		var _this = this;
+    		volunteering.forEach(function(vol) {
+    			_this.echo("[[b;;]● " + vol.type + ":]")
+    			_this.echo("[[i;;]" + vol.period + "]" + "\t" + vol.position + " of " + vol.name);
+    		});
+    	},
+    	languages: function() {
+    		var _this = this;
+    		languages.forEach(function(l) {
+    			_this.echo("[[b;;]● " + l.lang + "]" + "\t" + "[[i;;]" + l.level + "]");
+    		});
+    	},
+    	interests: function() {
+    		var _this = this;
+    		interests.forEach(function(i) {
+    			_this.echo('\t -' + i);
+    		});
+    	},
+    	github: function() {
+    		this.echo("opening github page...")
+			setTimeout(function(){
+					window.open(githubUrl,'_blank');
+				}, 1000);
+    	},
+        js: {
+
+        }
+    }, {
+    	checkArity: false,
+    	completion: true,
+    	greetings: greetingMsg,
+        height: 150,
+        width: $(window).width(),
+        onCommandNotFound: function(command, terminal) {
+            if(terminal.level()==1)
+    	       terminal.error("'"+command+"' is not right command");
+            else if (terminal.level()==2) {
+                if (command !== '') {
+                    var result = window.eval(command);
+                    if (result != undefined) {
+                        terminal.echo(String(result));
+                    }
+                }
+            }
+        },
+        //onResize: function(terminal) {
+       // 	terminal.resize([$(window).width(),$(window).height()]);
+       // 	terminal.reset();
+        //},
+        prompt: "[[b;#00c40d;]ali-karami.ir> ]"
+    });
+});
